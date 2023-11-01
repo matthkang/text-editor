da636e0d31ef4885aa69bc1285f781af69eac893
matthew <matthkang@gmail.com>
2023-10-31 22:19:58 -0700

    fix error, console value

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -7,15 +7,48 @@ const initdb = async () =>
         console.log('jate database already exists');
         return;
       }
-      db.createObjectStore('jate', { keyPath: 'id', autoIncrement: true });
+      db.createObjectStore('jate', { keyPath: "id", autoIncrement: true });
       console.log('jate database created');
     },
   });
 
-// TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+// add logic to a method that accepts some content and adds it to the database
+export const putDb = async (content) => {
+  console.log('PUT to the database');
 
-// TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+  // Create a connection to the database database and version we want to use.
+  const textDb = await openDB('jate', 1);
+
+  // Create a new transaction and specify the database and data privileges.
+  const tx = textDb.transaction('jate', 'readwrite');
+
+  // Open up the desired object store.
+  const store = tx.objectStore('jate');
+  const request = store.put({ id: 1, value: content });
+  const result = await request;
+  console.log('🚀 - data saved to the database', result);
+}
+
+// add logic for a method that gets all the content from the database
+export const getDb = async () => {
+  console.log('GET from the database');
+
+  // Create a connection to the database database and version we want to use.
+  const textDb = await openDB('jate', 1);
+
+  // Create a new transaction and specify the database and data privileges.
+  const tx = textDb.transaction('jate', 'readonly');
+
+  // Open up the desired object store.
+  const store = tx.objectStore('jate');
+
+  // Use the .getAll() method to get all data in the database.
+  const request = store.get(1);
+
+  // Get confirmation of the request.
+  const result = await request;
+  
+  return result.value;
+};
 
 initdb();
